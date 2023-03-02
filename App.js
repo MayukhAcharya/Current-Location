00a5bbfa1dfdbc436673fb6812feeeba1b2a4b8a
Max07092001 <mayukhacharya2001@gmail.com>
2023-03-02 23:41:32 +0530

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setlocation] = useState("");
+
+  const Getlocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      console.log("Permission to access location was denied");
+      return;
+    }
+    let currentLocation = await Location.getCurrentPositionAsync({});
+    setlocation(currentLocation);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location && (
+        <>
+          <Text>Your Latitude : {location.coords.latitude}</Text>
+          <Text>Your Longitude : {location.coords.longitude}</Text>
+        </>
+      )}
+      <Button title="Get Location" onPress={Getlocation} />
     </View>
   );
 }
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
