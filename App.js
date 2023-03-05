28e27f8c940f2fb45a5d24f15d4bfadeafb2cb01
Max07092001 <mayukhacharya2001@gmail.com>
2023-03-05 23:15:23 +0530

    Added map

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
-  const [location, setlocation] = useState("");
+  const [mapregion, Setmapregion] = useState({
+    latitude: 20.5937,
+    longitude: 78.9629,
+    latitudeDelta: 30,
+    longitudeDelta: 30,
+  });
 
   const Getlocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -11,18 +17,25 @@ export default function App() {
       console.log("Permission to access location was denied");
       return;
     }
-    let currentLocation = await Location.getCurrentPositionAsync({});
-    setlocation(currentLocation);
+    let currentLocation = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.BestForNavigation,
+    });
+    Setmapregion({
+      latitude: currentLocation.coords.latitude,
+      longitude: currentLocation.coords.longitude,
+      latitudeDelta: 0.000918,
+      longitudeDelta: 0.000418,
+    });
   };
   return (
     <View style={styles.container}>
-      {location && (
-        <>
-          <Text>Your Latitude : {location.coords.latitude}</Text>
-          <Text>Your Longitude : {location.coords.longitude}</Text>
-        </>
-      )}
       <Button title="Get Location" onPress={Getlocation} />
+      <MapView
+        style={{ height: "50%", width: "100%", margin: 10 }}
+        region={mapregion}
+      >
+        <Marker coordinate={mapregion} title="Your are here" draggable={true} />
+      </MapView>
     </View>
   );
 }
